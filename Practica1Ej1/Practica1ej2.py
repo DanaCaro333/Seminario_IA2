11ae7dd15f77cf1a8e396f5568c87db268b9277c
Dana Ramirez <dana.caro.ram@gmail.com>
2023-09-15 16:30:53 -0600

    Segunda parte

--- Practica1Ej1/Practica1ej2.py
+++ Practica1Ej1/Practica1ej2.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+from os import remove
+
 """
 Created on Thu Sep 14 22:08:37 2023
 
@@ -23,7 +25,39 @@ def particionar(dataset, e, pTrain, name):
         trainData.to_csv("./info/"+name+str(i+1)+"_trn.csv",header=False, index=False)
         testData.to_csv("./info/"+name+str(i+1)+"_tst.csv",header=False, index=False)
 
-#particionar("./info/spheres1d10.csv", 5, 80, "Ej2P1_")
 
-particionar("./info/spheres1d10.csv", 5, 80, "Ej2P1_")
+def merge(e):
+    for i in range(e):
+        
+        particionar("./info/spheres2d10.csv", 1, 80, "10mix_")
+        particionar("./info/spheres2d50.csv", 1, 80, "50mix_")
+        particionar("./info/spheres2d70.csv", 1, 80, "70mix_") 
+        
+        df10 = pd.DataFrame(pd.read_csv("./info/10mix_1_trn.csv", header=None))
+        df50 = pd.DataFrame(pd.read_csv("./info/50mix_1_trn.csv", header=None))
+        df70 = pd.DataFrame(pd.read_csv("./info/50mix_1_trn.csv", header=None))
+        
+        df10.merge(df50, how='inner')
+        df10.merge(df70, how='inner')
+        
+        df10.to_csv("./info/Mix"+str(i+1)+"_trn.csv",header=False, index=False)
+        
+        df10 = pd.DataFrame(pd.read_csv("./info/10mix_1_tst.csv", header=None))
+        df50 = pd.DataFrame(pd.read_csv("./info/50mix_1_tst.csv", header=None))
+        df70 = pd.DataFrame(pd.read_csv("./info/50mix_1_tst.csv", header=None))
+        
+        df10.merge(df50, how='inner')
+        df10.merge(df70, how='inner')
+        
+        remove("./info/10mix_1_trn.csv")
+        remove("./info/50mix_1_trn.csv")
+        remove("./info/70mix_1_trn.csv")
+        
+        remove("./info/10mix_1_tst.csv")
+        remove("./info/50mix_1_tst.csv")
+        remove("./info/70mix_1_tst.csv")
+        
+        df10.to_csv("./info/Mix"+str(i+1)+"_tst.csv",header=False, index=False)
+        
 
+merge(5)
